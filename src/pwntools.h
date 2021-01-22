0a7d547dc65160c06081892da53c03e964addca1
rajvardhan <rajvardhan1011@gmail.com>
2021-01-22 22:56:41 +0530

    fix indentation

--- src/pwntools.h
+++ src/pwntools.h
@@ -15,49 +15,48 @@
 #define I386 86
 
 namespace neolib{
-
-           void pause() {
-                std::cout << "(pause) Press enter to release." << std::endl;
-                getchar();
-            }
+    void pause() {
+        std::cout << "(pause) Press enter to release." << std::endl;
+        getchar();
+    }
 }
 
 namespace pack {
-      std::string p64(uint64_t n) {
+    std::string p64(uint64_t n) {
         return std::string((char *)&n, 8);
-      }
+    }
 
-      uint64_t u64(const std::string &buf) {
+    uint64_t u64(const std::string &buf) {
         assert(buf.length() == 8);
         return *(uint64_t *)buf.c_str();
-      }
+    }
 
-      std::string p32(uint32_t n) {
+    std::string p32(uint32_t n) {
         return std::string((char *)&n, 4);
-      }
+    }
 
-      uint32_t u32(const std::string &buf) {
+    uint32_t u32(const std::string &buf) {
         assert(buf.length() == 4);
         return *(uint32_t *)buf.c_str();
-      }
+    }
 
-      std::string flat(std::vector<uint64_t> chain) {
+    std::string flat(std::vector<uint64_t> chain) {
         std::string s;
         for(int i=0; i<chain.size(); ++i)
             s += pack::p64(chain[i]);
         return s;
-      }
+    }
 
-      std::string flat(std::vector<uint32_t> chain) {
+    std::string flat(std::vector<uint32_t> chain) {
         std::string s;
         for(int i=0; i<chain.size(); ++i)
             s += pack::p32(chain[i]);
         return s;
-      }
+    }
 
-      std::string construct_file(FILE *fp) {
-      return std::string((char *)fp, sizeof(FILE));
-  }
+    std::string construct_file(FILE *fp) {
+        return std::string((char *)fp, sizeof(FILE));
+    }
 };
 
 #endif
