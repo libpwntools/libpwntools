7aae2a1c27ebde23573abf8e3c05b5b5e4162339
harsh khuha <hkrawbytes@gmail.com>
2021-01-29 00:29:03 +0530

    add proper file handling

---
+++ examples/file_example/exp.cc
@@ -0,0 +1,11 @@
+#include <pwntools>
+#include <vector>
+#include <iostream>
+
+int main() {
+	file f;
+	f.fp->_fileno = 0x41;
+
+	std::string fake_stream = f.construct();
+	hexdump((void *)fake_stream.c_str(),fake_stream.size());
+}
