14e2145d967f89b156fcb80b93a980eada9ca8e6
harsh khuha <hkrawbytes@gmail.com>
2021-01-29 10:22:38 +0530

    add sigreturn frame example, fix sigreturn frame

--- examples/sropexample/exp.cc
+++ examples/sropexample/exp.cc
@@ -3,5 +3,12 @@
 #include <iostream>
 
 int main() {
-	
+	SigReturnFrame frame("amd64","amd64");
+
+	frame.rbp = 0x41414141;
+	frame.r9 = 0x77;
+
+	std::string fake_frame_bytes = frame.construct_frame();
+	hexdump(fake_frame_bytes);
+	return 0;
 }
