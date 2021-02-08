2c19aedf05ce1b842b22c39dc6d849285a00e3db
r4j0x00 <rajvardhan1011@gmail.com>
2021-02-09 02:49:30 +0530

    Fix styling issues

--- src/utils.cc
+++ src/utils.cc
@@ -8,56 +8,53 @@ bool ends_with(const std::string& a, const std::string& b) {
     return std::equal(a.begin() + a.size() - b.size(), a.end(), b.begin());
 }
 
-void hexdump_wrap(void *pAddressIn, size_t  lSize)
-{
- char szBuf[100];
- long lIndent = 1;
- long lOutLen, lIndex, lIndex2, lOutLen2;
- long lRelPos;
- struct { char *pData; unsigned long lSize; } buf;
- unsigned char *pTmp,ucTmp;
- unsigned char *pAddress = (unsigned char *)pAddressIn;
+void hexdump_wrap(void *pAddressIn, size_t  lSize) {
+    char szBuf[100];
+    long lIndent = 1;
+    long lOutLen, lIndex, lIndex2, lOutLen2;
+    long lRelPos;
+    struct { char *pData; unsigned long lSize; } buf;
+    unsigned char *pTmp,ucTmp;
+    unsigned char *pAddress = (unsigned char *)pAddressIn;
 
-   buf.pData   = (char *)pAddress;
-   buf.lSize   = lSize;
+    buf.pData   = (char *)pAddress;
+    buf.lSize   = lSize;
 
-   while (buf.lSize > 0)
-   {
-      pTmp     = (unsigned char *)buf.pData;
-      lOutLen  = (int)buf.lSize;
-      if (lOutLen > 16)
-          lOutLen = 16;
+    while (buf.lSize > 0) {
+        pTmp = (unsigned char *)buf.pData;
+        lOutLen = (int)buf.lSize;
 
-      // create a 64-character formatted output line:
-      sprintf(szBuf, " >                            "
+        if(lOutLen > 16)
+            lOutLen = 16;
+
+        sprintf(szBuf, " >                            "
                      "                      "
                      "    %08lX", pTmp-pAddress);
-      lOutLen2 = lOutLen;
+        lOutLen2 = lOutLen;
 
-      for(lIndex = 1+lIndent, lIndex2 = 53-15+lIndent, lRelPos = 0;
-          lOutLen2;
-          lOutLen2--, lIndex += 2, lIndex2++
-         )
-      {
-         ucTmp = *pTmp++;
+        for(lIndex = 1+lIndent, lIndex2 = 53-15+lIndent, lRelPos = 0; lOutLen2; lOutLen2--, lIndex += 2, lIndex2++) {
+            ucTmp = *pTmp++;
+            sprintf(szBuf + lIndex, "%02X ", (unsigned short)ucTmp);
 
-         sprintf(szBuf + lIndex, "%02X ", (unsigned short)ucTmp);
-         if(!isprint(ucTmp))  ucTmp = '.'; // nonprintable char
-         szBuf[lIndex2] = ucTmp;
+            if(!isprint(ucTmp))
+                ucTmp = '.';
 
-         if (!(++lRelPos & 3))     // extra blank after 4 bytes
-         {  lIndex++; szBuf[lIndex+2] = ' '; }
-      }
+            szBuf[lIndex2] = ucTmp;
+            if (!(++lRelPos & 3)) {
+                lIndex++;
+                szBuf[lIndex+2] = ' ';
+            }
+        }
 
-      if (!(lRelPos & 3)) lIndex--;
+        if (!(lRelPos & 3))
+            lIndex--;
 
-      szBuf[lIndex  ]   = '<';
-      szBuf[lIndex+1]   = ' ';
+        szBuf[lIndex  ]   = '<';
+        szBuf[lIndex+1]   = ' ';
 
-      printf("%s\n", szBuf);
-
-      buf.pData   += lOutLen;
-      buf.lSize   -= lOutLen;
+        printf("%s\n", szBuf);
+        buf.pData   += lOutLen;
+        buf.lSize   -= lOutLen;
     }
 }
 
