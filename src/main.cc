0e4f878b286a884916eb2a3414d244ccd0ebcb06
r4j0x00 <rajvardhan1011@gmail.com>
2021-02-09 20:10:40 +0530

    -

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main(void) {
-    fmtstr_payload p(6);
+    fmtstr_payload p(7);
     p[0x404018] = 0x4142434445464748;
     p[0x404020] = 0x5152535455565758;
     std::cout << p.build();
