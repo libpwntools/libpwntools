da313f586298b8ff9db024fd5de1ff17fba9861b
rajvardhan <rajvardhan1011@gmail.com>
2021-02-10 19:11:42 +0530

    Update utils.cc

--- src/utils.cc
+++ src/utils.cc
@@ -3,7 +3,7 @@
 #include <ostream>
 #include <bits/stdc++.h> 
 
-inline bool ends_with(const std::string& a, const std::string& b) {
+bool ends_with(const std::string& a, const std::string& b) {
     if (b.size() > a.size()) return false;
     return std::equal(a.begin() + a.size() - b.size(), a.end(), b.begin());
 }
