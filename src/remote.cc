7d1c4238c3618d4cf32bd16644a3d90c08a661e1
rajvardhan <rajvardhan1011@gmail.com>
2021-02-13 02:40:19 +0530

    update remote.cc and remote.h

--- src/remote.cc
+++ src/remote.cc
@@ -1,14 +1,28 @@
 #include <libpwntools/remote.h>
+#include <libpwntools/logger.h>
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <cassert>
 #include <thread>
 
-size_t send_wrapper(int fd, const char * buf, size_t len, int z) {
-    return send(fd, buf, len, z);
+#ifdef __linux__
+int send_wrapper(int f, const char * buf, size_t len, int z) {
+    return send(f, buf, len, z);
+}
+int recv_wrapper(int f, char* buf, size_t len, int z) {
+    return recv(f, buf, len, z);
+}
+#elif _WIN32
+int send_wrapper(SOCKET f, const char* buf, size_t len, int z) {
+    return send(f, buf, len, z);
 }
 
-size_t recv_wrapper(int fd, char * buf, size_t len, int z) {
-    return recv(fd, buf, len, z);
+int recv_wrapper(SOCKET f, char* buf, size_t len, int z) {
+    return recv(f, buf, len, z);
 }
+#endif
+
 
 pwn::Remote::Remote() {}
 
@@ -24,13 +38,21 @@ pwn::Remote::Remote(const std::string &ip, uint32_t port_number) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port_number);
 
+#ifdef _WIN32
+    int res = 0;
+    res = WSAStartup(MAKEWORD(2, 2), &this->wsaData);
+    if (res != 0) {
+        pwn::log::error("WSAStartup failed with error: " + std::to_string(iResult));
+        exit(1);
+    }
+#endif
     hostent * record = gethostbyname(ip.c_str());
-	if(record == nullptr) {
-		std::cerr << ip << " is unavailable\n";
-		exit(-1);
-	}
-
+    if(record == nullptr) {
+        std::cerr << ip << " is unavailable\n";
+        exit(-1);
+    }
 	serv_addr.sin_addr = *(in_addr *)record->h_addr;
+
     if((this->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         std::cerr << "Error while creating socket\n";
         exit(-1);
@@ -40,26 +62,47 @@ pwn::Remote::Remote(const std::string &ip, uint32_t port_number) {
         std::cerr << "Connection failed\n";
         exit(-1);
     }
-
     this->debug = false;
 }
 
 std::string pwn::Remote::recv_raw(size_t len) {
 	char * buf = (char *)malloc(len);
+#ifdef __linux__
 	len = recv_wrapper(this->fd, buf, len, 0);
+#elif _WIN32
+    len = recv_wrapper(this->fd, buf, len, 0);
+#endif
 	std::string s(buf, len);
 	free(buf);
 	return s;
 }
 
 size_t pwn::Remote::send(const std::string &data) {
-    if(this->debug) {
+    if (this->debug) {
         std::cout << "(Send)\n";
         pwn::hexdump(data);
     }
-    return send_wrapper(this->fd, data.c_str(), data.size(), 0);
+    size_t res;
+#ifdef _WIN32
+    res = send_wrapper(this->fd, data.c_str(), data.size(), 0);
+    if (res == SOCKET_ERROR) {
+        std::cout << "send failed with error : " << WSAGetLastError() << std::endl;
+        closesocket(this->fd);
+        WSACleanup();
+        exit(1);
+    }
+#elif __linux__
+    res = send_wrapper(this->fd, data.c_str(), data.size(), 0);
+    if (res < 0) {
+        std::cout << "send failed" << std::endl;
+        this->close();
+        exit(1);
+    }
+#endif
+    return res;
 }
 
+#ifdef __linux__
 void pwn::Remote::shutdown(const std::string& h) {
     int how;
 
@@ -77,7 +120,11 @@ void pwn::Remote::shutdown(const std::string& h) {
         exit(1);
     }
 }
-
+#endif
 void pwn::Remote::close() {
+#ifdef __linux__
 	::close(this->fd);
+#elif _WIN32
+    closesocket(this->fd);
+#endif
 }
--- src/remote.h
+++ src/remote.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
 #include <libpwntools/remote.h>
 #include <libpwntools/io.h>
 #include <libpwntools/utils.h>
+#ifdef __linux__
 #include <netdb.h>
 #include <sys/param.h>
 #include <netinet/in.h>
-#include <fstream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#elif _WIN32
+#include <windows.h>
+#include <winsock.h>
+#pragma comment (lib, "Ws2_32.lib")
+#endif
 
 #ifndef REMOTE_H
 #define REMOTE_H
@@ -19,14 +26,21 @@ namespace pwn {
         private:
             std::string host;
             std::string port;
+#ifdef __linux__
             int fd;
+#elif _WIN32
+            WSADATA wsaData;
+            SOCKET fd;
+#endif
         public:
             Remote();
             ~Remote();
             Remote(const std::string&, uint32_t);
             virtual std::string recv_raw(size_t) override;
             virtual size_t send(const std::string&) override;
-            void shutdown(const std::string& );
+#ifdef __linux__
+            void shutdown(const std::string&);
+#endif
             virtual void close() override;
     };
 }
